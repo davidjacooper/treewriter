1633926d2237eabb135ef6a1184bb5443b498674
David Cooper <david.cooper+git@djac.au>
2023-11-17 14:46:08 +0800

    Reintegrated TreeWriter and PrintTreeWriter, making TreeWriter extend PrintWriter, and aggregate (rather than extend) PrefixingWriter.

--- src/main/java/TreeWriter.java
+++ src/main/java/TreeWriter.java
@@ -2,24 +2,35 @@ package au.djac.jprinttree;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.function.*;
 
-public class TreeWriter extends PrefixingWriter
+public class TreeWriter extends PrintWriter
 {
     private TreeOptions options = new TreeOptions();
+    private PrefixingWriter prefixOut;
 
     public TreeWriter(OutputStream out, Charset charset)
     {
-        super(out, charset);
+        super(new PrefixingWriter(out, charset));
+        this.prefixOut = (PrefixingWriter) this.out;
     }
 
     public TreeWriter(OutputStream out)
     {
-        super(out);
+        this(out, Charset.defaultCharset());
     }
 
     public TreeWriter(Writer out)
     {
+        super(new PrefixingWriter(out));
+        this.prefixOut = (PrefixingWriter) this.out;
+    }
+
+    public TreeWriter(PrefixingWriter out)
+    {
         super(out);
+        this.prefixOut = out;
     }
 
     public TreeOptions options()
@@ -27,45 +38,78 @@ public class TreeWriter extends PrefixingWriter
         return options;
     }
 
-    public void startNode(boolean lastSibling) throws IOException
+    public void startNode(boolean lastSibling)
     {
-        if(lineLength > 0)
+        try
         {
-            lineBreak();
-        }
-
-        String connector, padding;
-        if(lastSibling)
-        {
-            connector = options.getLastConnector();
-            padding = options.getLastPaddingPrefix();
-        }
-        else
-        {
-            connector = options.getConnector();
-            padding = options.getPaddingPrefix();
-        }
-
-        int preNodeLines = options.getPreNodeLines();
-        if(preNodeLines == 0)
-        {
-            addPrefix(connector);
-            replacePrefixAfterLine(padding);
-        }
-        else
-        {
-            addPrefix(options.getParentLine());
-            for(int i = 0; i < preNodeLines; i++)
+            if(prefixOut.getLineLength() > 0)
             {
-                lineBreak();
+                prefixOut.lineBreak();
             }
-            replacePrefix(connector);
-            replacePrefixAfterLine(padding);
+
+            String connector, padding;
+            if(lastSibling)
+            {
+                connector = options.getLastConnector();
+                padding = options.getLastPaddingPrefix();
+            }
+            else
+            {
+                connector = options.getConnector();
+                padding = options.getPaddingPrefix();
+            }
+
+            int preNodeLines = options.getPreNodeLines();
+            if(preNodeLines == 0)
+            {
+                prefixOut.addPrefix(connector);
+                prefixOut.replacePrefixAfterLine(padding);
+            }
+            else
+            {
+                prefixOut.addPrefix(options.getParentLine());
+                for(int i = 0; i < preNodeLines; i++)
+                {
+                    prefixOut.lineBreak();
+                }
+                prefixOut.replacePrefix(connector);
+                prefixOut.replacePrefixAfterLine(padding);
+            }
+        }
+        catch(IOException e)
+        {
+            setError();
         }
     }
 
     public void endNode()
     {
-        removePrefix();
+        prefixOut.removePrefix();
+    }
+
+    public <N> void printTree(N node,
+                              Function<? super N,Collection<? extends N>> childFn,
+                              Function<? super N,String> nodeToString)
+    {
+        forTree(node, childFn, n -> print(nodeToString.apply(n)));
+        println();
+    }
+
+    public <N> void forTree(N node,
+                            Function<? super N,Collection<? extends N>> childFn,
+                            Consumer<? super N> nodePrinter)
+    {
+        nodePrinter.accept(node);
+
+        var children = childFn.apply(node);
+        int i = children.size();
+
+        for(var child : children)
+        {
+            startNode(i == 1);
+            forTree(child, childFn, nodePrinter);
+            endNode();
+            i--;
+        }
     }
 }
